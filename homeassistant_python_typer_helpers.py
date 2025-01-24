bfe67c456a059f48bb90e1f8320f43242645af41
Thomas BESSOU <thomas.bessou@hotmail.fr>
2025-01-24 23:57:15 +0100

    Add runtime checks that we don't get None states (against typing)
    so that if entity doesn't exist it does not silent fail and propagate None that don't match type into applicative code

--- homeassistant_python_typer_helpers.py
+++ homeassistant_python_typer_helpers.py
@@ -171,6 +171,8 @@ class Entity:
                 return self.hapt.state_cache[self.entity_id]
             else:
                 entity_state = self.query_state(attribute=None, copy=True)
+                if entity_state is None:  # pyright:ignore[reportUnnecessaryComparison]
+                    raise ValueError(f"{self.entity_id} not found")
                 self.hapt.state_cache[self.entity_id] = entity_state
                 return entity_state
         else:
@@ -179,6 +181,8 @@ class Entity:
                 entity_state = self.hapt.full_cache[self.entity_id]
             else:
                 entity_state = self.query_state(attribute="all", copy=True)
+                if entity_state is None:  # pyright:ignore[reportUnnecessaryComparison]
+                    raise ValueError(f"{self.entity_id} not found")
                 self.hapt.full_cache[self.entity_id] = entity_state
                 self.hapt.state_cache[self.entity_id] = entity_state["state"]
 
